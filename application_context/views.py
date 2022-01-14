978e3fccb87e5fa6ccd748bcd8d577ceed638fb7
sepehr-gh <sepehrgh01@gmail.com>
2022-01-14 11:07:34 +0330

    feat: add gunicorn and no_context example

--- application_context/views.py
+++ application_context/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+no_context_int: int = 0
+
+def no_context_view(request):
+    global no_context_int
+    no_context_int += 1
+    return HttpResponse("i=%s" % str(no_context_int))
